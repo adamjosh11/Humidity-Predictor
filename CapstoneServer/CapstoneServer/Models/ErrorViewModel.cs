f90649454e5b02092fb714a50a1179bca25776b7
Josh Adams <adamsjos02@gmail.com>
2023-09-26 09:05:17 -0600

    Server

---
+++ CapstoneServer/CapstoneServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CapstoneServer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
